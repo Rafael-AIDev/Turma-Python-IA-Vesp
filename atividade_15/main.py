8159820a255caf0626a533a840b5a6f6e8f59010
Rafael-AIDev <rafael.it.ai.br@gmail.com>
2024-08-09 17:36:39 -0300

    aula12

---
+++ atividade_15/main.py
@@ -0,0 +1,21 @@
+'''Crie um CRUD de nomes. Ou seja, um programa que:
+- Cadastre um nome.
+- Pesquise pelos nomes cadastrados.
+- Altere um nome.
+- Delete um nome da lista.
+
+O usuário poderá inserir quantos nomes quiser.'''
+
+nomes = []
+
+while True:
+    print('1 - Cadastre um nome')
+    print('1 - Pesquisa pelos nomes cadastrados')
+    print('1 - Alterar um nome')
+    print('1 - Deletar um nome')
+    print('1 - Sair do programa')
+
+    # menu
+    opcao = input('\nOpção do usuário: \n')
+
+            
